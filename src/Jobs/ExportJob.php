714e38411b25803b4e78c052716da15fb1630d55
sebastienfontaine <45472257+sebastienfontaine@users.noreply.github.com>
2021-12-05 13:09:12 +0100

    added job feature (#2)
    * added job support
    
    * updated pr
    
    * updated pr
    
    * updated pr
    
    * updated to php 8
    
    * fixed system when empty

---
+++ src/Jobs/ExportJob.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Metrichawk\MetrichawkLaravel\Jobs;
+
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Metrichawk\MetrichawkLaravel\Services\CollectorService;
+
+class ExportJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var int
+     */
+    public $timeout = 300;
+
+    /**
+     * @var string
+     */
+    private $dsn;
+
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * ExportJob constructor.
+     *
+     * @param string $dsn
+     * @param array  $data
+     */
+    public function __construct(string $dsn, array $data)
+    {
+        $this->dsn  = $dsn;
+        $this->data = $data;
+    }
+
+    public function handle(): void
+    {
+        if ($this->data['records']['queries'] !== null) {
+            $this->data['records']['queries'] = resolve(CollectorService::class)->formatQueryData($this->data['records']['queries']);
+        }
+
+        try {
+            retry(5, function () {
+                resolve(CollectorService::class)->sendData($this->dsn, $this->data);
+            }, 500);
+        } catch (Exception $e) {
+        }
+    }
+}
---
+++ src/Services/CollectorService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Metrichawk\MetrichawkLaravel\Services;
+
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Collection;
+
+class CollectorService
+{
+    /**
+     * @param string $dsn
+     * @param array  $data
+     */
+    public function sendData(string $dsn, array $data): void
+    {
+        $client = new Client([
+            'verify'  => false,
+            'timeout' => 10,
+        ]);
+
+        try {
+            $client->post($dsn, [
+                'json' => $data,
+            ]);
+        } catch (GuzzleException $e) {
+        } catch (Exception $exception) {
+        }
+    }
+
+    /**
+     * @param array $queryData
+     *
+     * @return array
+     */
+    public function formatQueryData(array $queryData): array
+    {
+        $data = [];
+
+        $queriesByConnection = collect($queryData)->groupBy('connection_name');
+
+        $queriesByConnection->each(function (Collection $queries, string $connectionName) use (&$data) {
+            $sqlDuration = $queries->sum('duration');
+
+            $sqlDuplicationCount = $queries->countBy('hash')->sum(function ($count) {
+                if ($count > 1) {
+                    return $count;
+                }
+
+                return 0;
+            });
+
+            $data[] = [
+                'connection_name'   => $connectionName,
+                'duration'          => $sqlDuration,
+                'duplication_count' => $sqlDuplicationCount,
+                'request_count'     => $queries->count(),
+            ];
+        });
+
+        return $data;
+    }
+}
